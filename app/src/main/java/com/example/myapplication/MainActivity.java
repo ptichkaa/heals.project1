74e50d7bb2fa547a6a3b69d395f6b9a036f113e3
Ptichka <a.patlia30@gmail.com>
2022-06-14 16:39:20 +0300

    Completed 3 tasks

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -8,73 +10,75 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
-    int stepsNumber = 0;
-    int stepsGoal = 9000;
-    int stepsIncrement = 111;
-    float percent = 0;
+    private int stepsNumber = 1;
+    private int stepsGoal = 100;
+    private int stepsIncrement = 1;
+    private float percent = 0;
 
-
-    TextView stepsAmountText;
-    TextView stepsGoalText;
-    TextView stepsPercent;
-    ProgressBar progressBar;
-
+    private TextView stepsAmountText;
+    private TextView stepsGoalText;
+    private TextView stepsPercent;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.e("TEST", "onCreate");
-        setContentView(R.layout.activity_main);
-        stepsAmountText = (TextView) findViewById(R.id.steps_amount);
-        stepsGoalText = (TextView) findViewById(R.id.steps_total);
-        stepsPercent = (TextView) findViewById(R.id.steps_percent);
-        progressBar = (ProgressBar) findViewById(R.id.steps_progress_bar);
+        Log.e("TEST", " start main Activity");
 
+        setContentView(R.layout.activity_main);
+        findView();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        stepsAmountText.setText(String.valueOf(stepsNumber));
-        stepsGoalText.setText(String.valueOf(stepsGoal));
-        stepsPercent.setText(String.valueOf(percent));
-        progressBar.setMax(stepsGoal);
-        Log.e("TEST", "onStart");
+
+        initDefaultValues();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e("TEST", "onResume");
+        Log.e("TEST", "onResume initListener");
 
+        initListener();
+    }
+
+    private void findView() {
+        stepsAmountText = (TextView) findViewById(R.id.steps_amount);
+        stepsGoalText = (TextView) findViewById(R.id.steps_total);
+        stepsPercent = (TextView) findViewById(R.id.steps_percent);
+        progressBar = (ProgressBar) findViewById(R.id.steps_progress_bar);
+    }
+
+    private void initListener() {
         stepsAmountText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 stepsNumber = stepsNumber + stepsIncrement;
                 stepsAmountText.setText(String.valueOf(stepsNumber));
                 stepsPercent.setText(String.valueOf(percent));
-                percent = stepsNumber / (float) stepsGoal;
+                percent = (stepsNumber / (float) stepsGoal);
                 progressBar.setProgress(stepsNumber);
-                Log.i("Test tag", "percent = " + percent);
+
+                Log.i("Test tag", "percent progress bar = " + percent);
+
+                if (stepsNumber>stepsGoal){
+                     Log.w("TAG", "maximum value exceeded = " + stepsGoal);
+                }
             }
-
         });
-
-
     }
 
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.e("TEST", "onPause");
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e("TEST", "onStop");
+    private void initDefaultValues() {
+        stepsAmountText.setText(String.valueOf(stepsNumber));
+        stepsGoalText.setText(String.valueOf(stepsGoal));
+        stepsPercent.setText(String.valueOf(percent));
+        progressBar.setMax(stepsGoal);
     }
 }
+
+
